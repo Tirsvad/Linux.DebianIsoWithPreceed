591f6002f7bcc5233eee89d1913e02cd676d0a41
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2023-06-11 11:21:40 +0200

    updated template VM Debian 12 VM Debian 11 archive Added argument parser

--- src/LinuxIsoWithPreseed/conf.sh
+++ src/LinuxIsoWithPreseed/conf.sh
@@ -6,7 +6,7 @@ declare -g TCLI_LINUXISOWITHPRESEED_PATH_VM=/srv/vm
 declare -g TCLI_LINUXISOWITHPRESEED_PATH_WORK=/var/tmp/tcli
 declare -g TCLI_LINUXISOWITHPRESEED_PATH_WORK_ISO=/var/tmp/tcli/iso
 
-
+# TODO Milestone 
 # This will have some network issue
 # declare -g TCLI_LINUXISOWITHPRESEED_VIRT_INSTALL_QEMU_CONNECT='qemu:///session'
 
